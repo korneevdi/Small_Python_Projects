5e4657ee4e562162f7f8b9bbc44fe3eb7e721750
Dmitrii Korneev <142536755+korneevdi@users.noreply.github.com>
2024-02-21 07:14:21 +0100

    Update Number_Systems.py

--- Number_Systems.py
+++ Number_Systems.py
@@ -7,61 +7,23 @@ Currently the program works for number systems with any bases from 2 to 16.
 """
 
 
-# definition of the function that converts numbers
+def convert(number, base1, base2):
+    # Convert the number to decimal number system
+    decimal_number = int(str(number), base1)
+    
+    # Convert the number from decimal number system
+    converted_number = ""
+    while decimal_number > 0:
+        remainder = decimal_number % base2
+        converted_number = str(remainder) + converted_number
+        decimal_number //= base2
+    
+    return converted_number
 
-def convert(a, b):
-    # Converting a number to decimal system
-    try:
-        c = int(c1)
-    except ValueError:
-        import sys
-        sys.exit("The number system base is an integer number from 2 to 16")
+# Application example
+n = input("Enter number: ")
+a = int(input("Enter initial number system base: "))
+b = int(input("Enter final number system base: "))
 
-    if c > 1 and c < 17:
-        b = int(a, base = c)
-    else:
-        import sys
-        sys.exit("The number system base is an integer number from 2 to 16")
-
-
-    # Converting a number from decimal system
-    c2 = input("Into which number system to convert: ")
-    try:
-        c = int(c2)
-    except ValueError:
-        import sys
-        sys.exit("The number system base is an integer number from 2 to 16")
-    F = []
-    while b >= c:
-        F.append(b%c)
-        b = b//c
-    F.append(b)
-    F.reverse()
-    G = []
-    for i in F:
-        if i < 10:
-            G.append(i)
-        elif i == 10:
-            G.append("a")
-        elif i == 11:
-            G.append("b")
-        elif i == 12:
-            G.append("c")
-        elif i == 13:
-            G.append("d")
-        elif i == 14:
-            G.append("e")
-        elif i == 15:
-            G.append("f")
-    res = ""
-    for i in G:
-        res += str(i)
-    print("The number in the system with base", c, ":", res)
-
-
-# User's input
-
-a = input("Enter your number: ")                # user enters the initial number
-c1 = input("Enter the number system: ")         # user enters the number system base
-
-convert(a,c1)
+m = convert(n, a, b)
+print("Number", n, "in the number system", b, "is:", m)
